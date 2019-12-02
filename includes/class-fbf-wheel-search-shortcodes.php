bcfc7216f80cc4e2633e65d7c9bc1eab301fea92
Kevin Price-Ward <kevin.price-ward@chapteragency.co.uk>
2019-12-02 10:05:57 +0000

    aouse change

--- includes/class-fbf-wheel-search-shortcodes.php
+++ includes/class-fbf-wheel-search-shortcodes.php
@@ -5,20 +5,16 @@ class Fbf_Wheel_Search_Shortcodes
 {
     public function wheel_search($atts)
     {
-        $html = '<div class="mb-5">';
-        $html.= Fbf_Wheel_Search_Public::manufacturers_dropdown();
+        $html= Fbf_Wheel_Search_Public::manufacturers_dropdown();
         $html.= Fbf_Wheel_Search_Public::chasis_dropdown();
-        $html.= '</div>';
 
         return $html;
     }
 
     public function package_search($atts)
     {
-        $html = '<div class="mb-5">';
-        $html.= Fbf_Wheel_Search_Public::manufacturers_dropdown_package();
+        $html= Fbf_Wheel_Search_Public::manufacturers_dropdown_package();
         $html.= Fbf_Wheel_Search_Public::chasis_dropdown_package();
-        $html.= '</div>';
 
         return $html;
     }
